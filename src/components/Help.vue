b1432c2b678ce796f7c603cad8e2aef2871d07bd
Aliaksei Krauchuk <aliaksei.krauchuk@bgaming.com>
2026-02-06 16:53:54 +0100

    Refactor Help.vue component to wrap tooltip slot in a span for improved styling and structure

--- src/components/Help.vue
+++ src/components/Help.vue
@@ -1,12 +1,16 @@
 <template>
-  <el-tooltip
-    :effect="$store.state.settings.tt.effect"
-    :content="text"
-    :placement="$store.state.settings.tt.placement"
-    :open-delay="$store.state.settings.tt.delay"
-  >
-    <slot></slot>
-  </el-tooltip>
+  <div>
+    <el-tooltip
+      :effect="$store.state.settings.tt.effect"
+      :content="text"
+      :placement="$store.state.settings.tt.placement"
+      :open-delay="$store.state.settings.tt.delay"
+    >
+      <span class="help-trigger">
+        <slot></slot>
+      </span>
+    </el-tooltip>
+  </div>
 </template>
 
 <script>
@@ -17,5 +21,4 @@
 </script>
 
 <style scoped>
-
 </style>
