e088d76d66cf939655ab0bd1e66f1e99f3b64379
Aliaksei Krauchuk <aliaksei.krauchuk@bgaming.com>
2026-02-06 17:09:07 +0100

    Refactor SidePanel.vue component to use Composition API

--- src/components/SidePanel.vue
+++ src/components/SidePanel.vue
@@ -1,25 +1,21 @@
 <template>
-
   <div class="container" layout="row left-top">
-
-
     <el-card class="box-card values" shadow="always">
-      <emitter-values v-if="emitterPreset != null" :data="emitterPreset"/>
-      <sequence-values v-if="sequencePreset != null" :data="sequencePreset"/>
+      <emitter-values v-if="emitterPreset != null" :data="emitterPreset" />
+      <sequence-values v-if="sequencePreset != null" :data="sequencePreset" />
     </el-card>
 
     <div>
       <el-card class="engine">
-        <engine-values v-if="bundleSettings != null" :data="bundleSettings"/>
+        <engine-values v-if="bundleSettings != null" :data="bundleSettings" />
       </el-card>
       <el-card class="tabs">
-
         <el-tabs type="" @tab-click="handleTabClick">
           <el-tab-pane label="Emitters">
-            <emitter-list/>
+            <emitter-list />
           </el-tab-pane>
           <el-tab-pane label="Sequences">
-            <sequence-list/>
+            <sequence-list />
           </el-tab-pane>
         </el-tabs>
       </el-card>
@@ -27,89 +23,101 @@
   </div>
 </template>
 
-<script>
-  import EmitterList from "./EmitterList.vue";
-  import EmitterValues from "./EmitterValues.vue";
-  import {EVENT_EMITTER_PRESET_REMOVED, EVENT_EMITTER_PRESET_SELECTED, EVENT_RESET, EVENT_SEQUENCE_PRESET_SELECTED} from "../events";
-  import EngineValues from "./EngineValues.vue";
-  import SequenceList from "./SequenceList.vue";
-  import SequenceValues from "./SequenceValues.vue";
+<script setup>
+defineOptions({ name: "SidePanel" });
+import { ref, computed, onMounted, onUnmounted, getCurrentInstance } from "vue";
+import { useStore } from "vuex";
+import EmitterList from "./EmitterList.vue";
+import EmitterValues from "./EmitterValues.vue";
+import EngineValues from "./EngineValues.vue";
+import SequenceList from "./SequenceList.vue";
+import SequenceValues from "./SequenceValues.vue";
+import {
+  EVENT_EMITTER_PRESET_REMOVED,
+  EVENT_EMITTER_PRESET_SELECTED,
+  EVENT_RESET,
+  EVENT_SEQUENCE_PRESET_SELECTED,
+} from "../events";
 
-  export default {
-    name: "SidePanel",
-    components: {SequenceValues, SequenceList, EngineValues, EmitterValues, EmitterList},
-    created() {
-      this.$eventBus.$on(EVENT_EMITTER_PRESET_SELECTED, this.onEmitterSelected);
-      this.$eventBus.$on(EVENT_EMITTER_PRESET_REMOVED, this.onEmitterRemoved);
-      this.$eventBus.$on(EVENT_SEQUENCE_PRESET_SELECTED, this.onSequenceSelected);
-      this.$eventBus.$on(EVENT_EMITTER_PRESET_REMOVED, this.onSequenceRemoved);
-    },
-    data() {
-      return {
-        emitterPreset: null,
-        sequencePreset: null
-      }
-    },
-    computed: {
-      bundleSettings() {
-        return this.$store.state.bundle;
-      }
-    },
-    methods: {
-      handleTabClick() {
-        console.log('tab!');
+const store = useStore();
+const instance = getCurrentInstance();
+const eventBus = instance.appContext.config.globalProperties.$eventBus;
+const editor = () => instance.appContext.config.globalProperties.$editor;
+const fx = () => instance.appContext.config.globalProperties.$fx;
 
-        this.$eventBus.$emit(EVENT_EMITTER_PRESET_SELECTED, null);
-        this.$eventBus.$emit(EVENT_SEQUENCE_PRESET_SELECTED, null);
-        this.$eventBus.$emit(EVENT_RESET);
-        this.$fx.stopAllEffects();
-      },
-      onEmitterSelected(preset) {
-        this.emitterPreset = preset;
-        this.sequencePreset = null;
-        this.$editor.layers.floorGizmoLayer.visible = true;
-      },
-      onEmitterRemoved(preset) {
-        if (this.emitterPreset === preset) {
-          this.emitterPreset = null;
-        }
-      },
-      onSequenceSelected(preset) {
-        this.emitterPreset = null;
-        this.sequencePreset = preset;
-        this.$editor.layers.floorGizmoLayer.visible = false;
-      },
-      onSequenceRemoved(preset) {
-        if (this.sequencePreset === preset) {
-          this.sequencePreset = null;
-        }
-      }
-    }
+const emitterPreset = ref(null);
+const sequencePreset = ref(null);
+
+const bundleSettings = computed(() => store.state.bundle);
+
+function onEmitterSelected(preset) {
+  emitterPreset.value = preset;
+  sequencePreset.value = null;
+  const ed = editor();
+  if (ed?.layers?.floorGizmoLayer) ed.layers.floorGizmoLayer.visible = true;
+}
+
+function onEmitterRemoved(preset) {
+  if (emitterPreset.value === preset) {
+    emitterPreset.value = null;
   }
+}
+
+function onSequenceSelected(preset) {
+  emitterPreset.value = null;
+  sequencePreset.value = preset;
+  const ed = editor();
+  if (ed?.layers?.floorGizmoLayer) ed.layers.floorGizmoLayer.visible = false;
+}
+
+function onSequenceRemoved(preset) {
+  if (sequencePreset.value === preset) {
+    sequencePreset.value = null;
+  }
+}
+
+function handleTabClick() {
+  eventBus.$emit(EVENT_EMITTER_PRESET_SELECTED, null);
+  eventBus.$emit(EVENT_SEQUENCE_PRESET_SELECTED, null);
+  eventBus.$emit(EVENT_RESET);
+  const fxInstance = fx();
+  if (fxInstance) fxInstance.stopAllEffects();
+}
+
+onMounted(() => {
+  eventBus.$on(EVENT_EMITTER_PRESET_SELECTED, onEmitterSelected);
+  eventBus.$on(EVENT_EMITTER_PRESET_REMOVED, onEmitterRemoved);
+  eventBus.$on(EVENT_SEQUENCE_PRESET_SELECTED, onSequenceSelected);
+  eventBus.$on(EVENT_EMITTER_PRESET_REMOVED, onSequenceRemoved);
+});
+
+onUnmounted(() => {
+  eventBus.$off(EVENT_EMITTER_PRESET_SELECTED, onEmitterSelected);
+  eventBus.$off(EVENT_EMITTER_PRESET_REMOVED, onEmitterRemoved);
+  eventBus.$off(EVENT_SEQUENCE_PRESET_SELECTED, onSequenceSelected);
+  eventBus.$off(EVENT_EMITTER_PRESET_REMOVED, onSequenceRemoved);
+});
 </script>
 
 <style scoped>
-  .container {
-    padding: 5px;
-  }
+.container {
+  padding: 5px;
+}
 
-  .engine {
-    height: 180px;
-  }
+.engine {
+  height: 180px;
+}
 
-  .tabs {
-    height: calc(100vh - 250px);
-    min-height: 200px;
-    width: 250px;
-  }
+.tabs {
+  height: calc(100vh - 250px);
+  min-height: 200px;
+  width: 250px;
+}
 
-  .values {
-    /*margin-top: 5px;*/
-    height: calc(100vh - 70px);
-    overflow-y: auto;
-    width: 300px;
-    margin-right: 5px;
-  }
-
-
+.values {
+  height: calc(100vh - 70px);
+  overflow-y: auto;
+  width: 300px;
+  margin-right: 5px;
+}
 </style>
